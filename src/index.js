88556e90e2547f5eea682cb24b331cb6252800ef
awmleer <awmleer@sparker.xyz>
2017-08-18 08:07:30 +0800

    [try]

---
+++ src/index.js
@@ -0,0 +1,18 @@
+'use strict';
+
+$.ready(function (error) {
+    if (error) {
+        console.error(error);
+        return;
+    }else {
+      console.log('hahaha');
+    }
+
+
+
+    // ...
+});
+
+$.end(function () {
+    // ...
+});

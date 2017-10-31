6e6caddfb6f2984f20e5f5611eb955641a66bfc5
awmleer <awmleer@sparker.xyz>
2017-10-31 21:14:00 +0800

    try webpack

--- client/src/index.js
+++ client/src/index.js
@@ -1,16 +1,18 @@
 'use strict';
 
+var  io = require('socket.io-client');
+var serverUrl = 'http://192.168.0.3/';
+var conn = io.connect(serverUrl);
+
 $.ready(function (error) {
     if (error) {
         console.error(error);
         return;
     }else {
-      console.log('hahaha');
+        // conn.emit();
+        console.log('hahaha');
     }
 
-
-
-    // ...
 });
 
 $.end(function () {

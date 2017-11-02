0bee5ae3fb0959a14ff7b818ae7cbb256844a9fd
awmleer <awmleer@sparker.xyz>
2017-11-02 20:33:37 +0800

    [+] net.socket

--- client/src/index.js
+++ client/src/index.js
@@ -3,7 +3,20 @@
 var document=null;
 // import io from 'socket.io-client';
 var serverUrl = 'http://192.168.0.3/';
-var socket = require('socket.io-client')(serverUrl);
+var net = require('net');
+
+var client = net.connect(6969,"192.168.0.3",
+    function() { //'connect' listener
+  console.log('connected to server!');
+  client.write('world!\r\n');
+});
+client.on('data', function(data) {
+  console.log(data.toString());
+  client.end();
+});
+client.on('end', function() {
+  console.log('disconnected from server');
+});
 
 $.ready(function (error) {
     if (error) {

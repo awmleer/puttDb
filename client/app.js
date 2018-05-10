9d3dd4497272b706622e8548262cc96f22680288
awmleer <awmleer@sparker.xyz>
2018-05-10 20:07:09 +0800

    socket.io pass documents

--- client/app.js
+++ client/app.js
@@ -1,6 +1,14 @@
 const socket = require('socket.io-client')('http://localhost:3000');
+const jsondiffpatch = require('jsondiffpatch').create();
+
 socket.on('connect', function(){
   console.log('connect');
+  socket.emit('subscribe', {
+    documentId: '5af42dee0dcefe76670de405'
+  }, (data) => {
+    console.log(data);
+  });
 });
 socket.on('event', function(data){});
 socket.on('disconnect', function(){});
+

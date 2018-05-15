c56a73ebb8d6224f84b79d19faa74e4e0e8552bf
awmleer <awmleer@sparker.xyz>
2018-05-15 08:34:43 +0800

    json() method

--- src/common/observable-document.ts
+++ src/common/observable-document.ts
@@ -7,31 +7,26 @@ export class ObservableDocument {
   public _valueProxy;
   private static proxyHandler:ProxyHandler<Object> = {
     get: (target:Object, p: PropertyKey, receiver: any):any => {
-      console.log('Get');
-      console.log(target);
-      console.log(p);
-      console.log(typeof p);
-      console.log(target.hasOwnProperty(p));
+      // console.log('Get');
+      // console.log(target);
+      // console.log(p);
+      // console.log(typeof p);
+      // console.log(target.hasOwnProperty(p));
       // console.log(receiver);
-      if(p==='toJSON'){
-        console.log('json!');
-        console.log(target['toJSON']);
-        return target;
-        // return JSON.stringify(target);
-      }
       if((typeof p === 'number' || typeof p === 'string') && target.hasOwnProperty(p)){
         if(Array.isArray(target[p])||target[p] instanceof Object){
           return new Proxy(target[p], ObservableDocument.proxyHandler);
         }else{
           return target[p];
         }
+      }else{
+        return target;
       }
-      return target;
     },
     set: (target: Object, p: PropertyKey, value: any, receiver: any):boolean => {
       target[p] = value;
-      console.log('Set');
-      console.log(target);
+      // console.log('Set');
+      // console.log(target);
       //TODO emit event
       return true;
     }
@@ -40,6 +35,7 @@ export class ObservableDocument {
   constructor(value:Object){
     this.value = value;
   }
+
   public get value():Object{
     return this._valueProxy;
   }
@@ -49,6 +45,10 @@ export class ObservableDocument {
     this._value = newValue;
 
     this._valueProxy = new Proxy(this._value, ObservableDocument.proxyHandler);
+  }
+
+  public get json():string{
+    return JSON.stringify(this._value);
   }
 
 }
